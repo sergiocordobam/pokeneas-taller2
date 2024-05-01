8382f832ef3543475184e38a593306424c6a1954
mstermigol <migueljaramillo879@gmail.com>
2024-05-01 12:33:21 -0500

    Add routes, controllers, views and other logic
    Co-authored-by: Sergio Córdoba <sergiocordobam@users.noreply.github.com>

--- src/models/Pokenea.js
+++ src/models/Pokenea.js
@@ -4,7 +4,7 @@ const pokeneas = [
         nombre: "Bulbasaurio",
         altura: 0.7,
         habilidad: "Espesura",
-        imagen: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/001.png",
+        imagen: "https://storage.googleapis.com/pokeneas-mja-scm/bulbasaur.png",
         fraseFilosofica: "Filosofando ando"
     },
     {
@@ -12,7 +12,7 @@ const pokeneas = [
         nombre: "ChArmando",
         altura: 0.6,
         habilidad: "Mar Llamas",
-        imagen: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/004.png",
+        imagen: "https://storage.googleapis.com/pokeneas-mja-scm/charmander.png",
         fraseFilosofica: "Si cierro los ojos no veo"
     },
     {
@@ -20,7 +20,7 @@ const pokeneas = [
         nombre: "Pidgey",
         altura: 0.3,
         habilidad: "Vista Lince",
-        imagen: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/016.png",
+        imagen: "https://storage.googleapis.com/pokeneas-mja-scm/pidgey.png",
         fraseFilosofica: "El deseo es la verdadera esencia del hombre"
     },
     {
@@ -28,7 +28,7 @@ const pokeneas = [
         nombre: "Caterpie",
         altura: 0.3,
         habilidad: "Polvo Escudo",
-        imagen: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/010.png",
+        imagen: "https://storage.googleapis.com/pokeneas-mja-scm/caterpie.png",
         fraseFilosofica: "La paciencia es amarga, pero su fruto es dulce"
     },
     {
@@ -36,7 +36,7 @@ const pokeneas = [
         nombre: "Pikachu",
         altura: 0.4,
         habilidad: "Elec. Estática",
-        imagen: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/025.png",
+        imagen: "https://storage.googleapis.com/pokeneas-mja-scm/pikachu.png",
         fraseFilosofica: "Hace falta una vida para aprender a vivir"
     },
     {
@@ -44,7 +44,7 @@ const pokeneas = [
         nombre: "Clefairy",
         altura: 0.6,
         habilidad: "Gran Encanto",
-        imagen: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/035.png",
+        imagen: "https://storage.googleapis.com/pokeneas-mja-scm/clefairy.png",
         fraseFilosofica: "Los grandes resultados requieren grandes ambiciones"
     },
     {
@@ -52,7 +52,7 @@ const pokeneas = [
         nombre: "Arcanine",
         altura: 1.9,
         habilidad: "Intimidación",
-        imagen: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/059.png",
+        imagen: "https://storage.googleapis.com/pokeneas-mja-scm/arcanine.png",
         fraseFilosofica: "El hombre es la medida de todas las cosas"
     },
     {
@@ -60,7 +60,7 @@ const pokeneas = [
         nombre: "Blastoise",
         altura: 1.6,
         habilidad: "Torrente",
-        imagen: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/009.png",
+        imagen: "https://storage.googleapis.com/pokeneas-mja-scm/blastoise.png",
         fraseFilosofica: "El obstáculo es el camino"
     },
     {
@@ -68,7 +68,7 @@ const pokeneas = [
         nombre: "Golem",
         altura: 1.4,
         habilidad: "Cabeza Roca",
-        imagen: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/076.png",
+        imagen: "https://storage.googleapis.com/pokeneas-mja-scm/golem.png",
         fraseFilosofica: "La libertad está en ser dueños de la propia vida"
     },
     {
@@ -76,7 +76,7 @@ const pokeneas = [
         nombre: "Ponyta",
         altura: 1.0,
         habilidad: "Fuga",
-        imagen: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/077.png",
+        imagen: "https://storage.googleapis.com/pokeneas-mja-scm/ponyta.png",
         fraseFilosofica: "Pienso, luego existo"
     },
 ]
