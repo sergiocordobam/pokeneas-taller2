3148cdfe72af56f0e5c137d3cf72c0899b9c11ff
mstermigol <migueljaramillo879@gmail.com>
2024-05-01 10:56:08 -0500

    Add model and basic app.js
    Co-authored-by: Sergio Córdoba <sergiocordobam@users.noreply.github.com>

---
+++ src/models/Pokenea.js
@@ -0,0 +1,83 @@
+const pokeneas = [
+    {
+        id: 1,
+        nombre: "Bulbasaurio",
+        altura: 0.7,
+        habilidad: "Espesura",
+        imagen: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/001.png",
+        fraseFilosofica: "Filosofando ando"
+    },
+    {
+        id: 2,
+        nombre: "ChArmando",
+        altura: 0.6,
+        habilidad: "Mar Llamas",
+        imagen: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/004.png",
+        fraseFilosofica: "Si cierro los ojos no veo"
+    },
+    {
+        id: 3,
+        nombre: "Pidgey",
+        altura: 0.3,
+        habilidad: "Vista Lince",
+        imagen: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/016.png",
+        fraseFilosofica: "El deseo es la verdadera esencia del hombre"
+    },
+    {
+        id: 4,
+        nombre: "Caterpie",
+        altura: 0.3,
+        habilidad: "Polvo Escudo",
+        imagen: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/010.png",
+        fraseFilosofica: "La paciencia es amarga, pero su fruto es dulce"
+    },
+    {
+        id: 5,
+        nombre: "Pikachu",
+        altura: 0.4,
+        habilidad: "Elec. Estática",
+        imagen: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/025.png",
+        fraseFilosofica: "Hace falta una vida para aprender a vivir"
+    },
+    {
+        id: 6,
+        nombre: "Clefairy",
+        altura: 0.6,
+        habilidad: "Gran Encanto",
+        imagen: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/035.png",
+        fraseFilosofica: "Los grandes resultados requieren grandes ambiciones"
+    },
+    {
+        id: 7,
+        nombre: "Arcanine",
+        altura: 1.9,
+        habilidad: "Intimidación",
+        imagen: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/059.png",
+        fraseFilosofica: "El hombre es la medida de todas las cosas"
+    },
+    {
+        id: 8,
+        nombre: "Blastoise",
+        altura: 1.6,
+        habilidad: "Torrente",
+        imagen: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/009.png",
+        fraseFilosofica: "El obstáculo es el camino"
+    },
+    {
+        id: 9,
+        nombre: "Golem",
+        altura: 1.4,
+        habilidad: "Cabeza Roca",
+        imagen: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/076.png",
+        fraseFilosofica: "La libertad está en ser dueños de la propia vida"
+    },
+    {
+        id: 10,
+        nombre: "Ponyta",
+        altura: 1.0,
+        habilidad: "Fuga",
+        imagen: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/077.png",
+        fraseFilosofica: "Pienso, luego existo"
+    },
+]
+
